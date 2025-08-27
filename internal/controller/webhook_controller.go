0281d59a65a833310cc7adc36a2e0e6a6ac790a9
Muhammed Aslam <aslam@keyvalue.systems>
2025-08-27 16:37:20 +0530

    update ack message

--- internal/controller/webhook_controller.go
+++ internal/controller/webhook_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -78,14 +79,8 @@ func (w *webhookController) processWebhook(c *gin.Context) {
 	allNames := append(append(updatedNames, failedNames...), skippedNames...)
 	w.logger.Info(c.Request.Context(), "HTTP request completed | endpoint: /webhook | status: "+strconv.Itoa(http.StatusOK)+" | servicesFound: "+strconv.Itoa(len(allNames))+" | servicesUpdated: "+strconv.Itoa(len(updatedNames))+" | servicesFailed: "+strconv.Itoa(len(failedNames))+" | servicesSkipped: "+strconv.Itoa(len(skippedNames)))
 
-	var message string
-	if len(updatedNames) > 0 {
-		message = "Webhook processed successfully. Services updated: " + strconv.Itoa(len(updatedNames))
-	} else if len(skippedNames) > 0 {
-		message = "Webhook processed successfully. No services required an update."
-	} else {
-		message = "Webhook processing failed. Services failed to update: " + strconv.Itoa(len(failedNames))
-	}
+	message := fmt.Sprintf("sent response: Webhook processing completed. Updated: %d ,skipped: %d, failed: %d", len(updatedNames), len(skippedNames), len(failedNames))
+	w.logger.Info(c.Request.Context(), message)
 
 	// Return structured response
 	response := dto.WebhookResponse{
